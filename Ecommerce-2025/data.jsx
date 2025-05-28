5f7a9fad2b76948d658f442b06e2fec0849a8d5e
Zhully <agunregezhuliakho@gmail.com>
2025-05-27 21:25:48 -0700

    ecommerce-2025

---
+++ Ecommerce-2025/data.jsx
@@ -0,0 +1,114 @@
+import headphone1 from './assets/head2.jpg'
+import headphone2 from './assets/head3.jpg'
+import headphone3 from './assets/head4.jpg'
+import headphone4 from './assets/head5.jpg'
+import headphone5 from './assets/head6.jpg'
+import headphone6 from './assets/head7.jpg'
+import headphone7 from './assets/head8.jpg'
+import headphone8 from './assets/head9.jpg'
+import headphone9 from './assets/head10.jpg'
+import headphone10 from './assets/head11.jpg'
+import headphone11 from './assets/head12.jpg'
+import headphone12 from './assets/head13.jpg'
+
+
+
+export const productsData = [
+    {
+        id : 1,
+        image : headphone1,
+        title : ' Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 2,
+        image : headphone2,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 3,
+        image : headphone3,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 4,
+        image : headphone4,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 5,
+        image : headphone5,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 6,
+        image : headphone6,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 7,
+        image : headphone7,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 8,
+        image : headphone8,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 9,
+        image : headphone9,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 10,
+        image : headphone10,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 11,
+        image : headphone11,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+    {
+        id : 12,
+        image : headphone12,
+        title : '  Noise Canceling Headphone',
+        price: 45,
+        category: 'ladies',
+        description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores nulla eos fuga maiores dolore, cum facere aperiam doloremque praesentium totam expedita quibusdam excepturi necessitatibus autem perspiciatis sunt eligendi minima quam.',
+    },
+
+]
